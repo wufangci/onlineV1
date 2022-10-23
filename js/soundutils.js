18adbdc7c3832f374c7e6722cda7764f396a583b
wufangci <cicichen0825@gmail.com>
2022-10-23 11:06:38 +0800

    add story cards

--- js/soundutils.js
+++ js/soundutils.js
@@ -1,21 +1,22 @@
 // 當點擊網頁的時候自動播放聲音
-AFRAME.registerComponent("firstsound", {
-  init: function () {
-    let sound = this.el.components.sound;
-    let soundloaded = false;
-    this.el.addEventListener("sound-loaded", function () {
-      soundloaded = true;
-    });
-    window.addEventListener("click", triggerSound, false);
-    window.addEventListener("touchstart", triggerSound, false);
-
-    function triggerSound() {
-      if (sound && soundloaded) {
-        sound.pauseSound();
-        sound.playSound();
-        window.removeEventListener("click", triggerSound, false);
-        window.removeEventListener("touchstart", triggerSound, false);
-      }
-    }
-  },
+AFRAME.registerComponent('firstsound', {
+    init: function () {
+        let sound = this.el.components.sound;
+        let soundloaded = false;
+        this.el.addEventListener('sound-loaded', function(){
+            soundloaded = true;
+        })
+        window.addEventListener('click', triggerSound, false);
+        window.addEventListener('touchstart', triggerSound, false);
+        
+        function triggerSound() {
+            if (sound && soundloaded) {
+                sound.pauseSound();
+                sound.playSound();
+                window.removeEventListener('click', triggerSound, false);
+                window.removeEventListener('touchstart', triggerSound, false);
+            }
+        }
+    },
 });
+
